c1008bc5b9ca27970a980be32af4cf1623dc5e96
pyshankov <pyshankov@gmail.com>
2016-05-24 16:16:14 +0300

    5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,32 +8,26 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
 
+        System.out.println("10000000000000000000001001".length());
+        System.out.println("100000000000000000001000111".length());
+        System.out.println("1000000000000000000000100111".length());
 
+//        long l1Px = 0b1000000000000000000000001010011l;
+//        long l2Px = 0b10000000000000000000000000001001l;
+//        long l3Px = 0b100000000000000000000000010101111l;
 
-        long l1Px = 0b1000000000000000000000001010011l;
-        long l2Px = 0b10000000000000000000000000001001l;
-        long l3Px = 0b100000000000000000000000010101111l;
-
-//            long l1Px= 0b10000000000000000000001001;
-//            long l2Px= 0b100000000000000000001000111;
-//            long l3Px= 0b1000000000000000000000100111;
-
-//        int l1Px = 0b100000000101;
-//        int l2Px = 0b1000011011;
-//        int l3Px = 0b10000001001;
-
-
-            long firstl1= 17451593;
-            long firstl2= 22162887;
-            long firstl3= 101019995;
+            long l1Px= 0b10000000000000000000001001;
+            long l2Px= 0b100000000000000000001000111;
+            long l3Px= 0b1000000000000000000000100111;
 
 
 
-        CryptoAnalysis.breakGeffe(l1Px,l2Px,l3Px,CryptoAnalysis.zi);
 
-        LinearFeedbackShiftRegister p1 = new LinearFeedbackShiftRegister(l1Px,1146);
-        LinearFeedbackShiftRegister p2 = new LinearFeedbackShiftRegister(l2Px,5);
-        LinearFeedbackShiftRegister p3 = new LinearFeedbackShiftRegister(l3Px,885);
+//        CryptoAnalysis.breakGeffe(l1Px,l2Px,l3Px,CryptoAnalysis.zi);
+
+        LinearFeedbackShiftRegister p1 = new LinearFeedbackShiftRegister(l1Px,17451593);
+        LinearFeedbackShiftRegister p2 = new LinearFeedbackShiftRegister(l2Px,22162887);
+        LinearFeedbackShiftRegister p3 = new LinearFeedbackShiftRegister(l3Px,101019995);
 
         StringBuilder b = new StringBuilder();
 
