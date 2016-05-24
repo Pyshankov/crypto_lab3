0ee4a5a6c34e711474605cd3ae8b3edef810bebe
pyshankov <pyshankov@gmail.com>
2016-05-24 20:00:11 +0300

    6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,18 +12,18 @@ public class Main {
         System.out.println("100000000000000000001000111".length());
         System.out.println("1000000000000000000000100111".length());
 
-//        long l1Px = 0b1000000000000000000000001010011l;
-//        long l2Px = 0b10000000000000000000000000001001l;
-//        long l3Px = 0b100000000000000000000000010101111l;
+        long l1Px = 0b1000000000000000000000001010011l;
+        long l2Px = 0b10000000000000000000000000001001l;
+        long l3Px = 0b100000000000000000000000010101111l;
 
-            long l1Px= 0b10000000000000000000001001;
-            long l2Px= 0b100000000000000000001000111;
-            long l3Px= 0b1000000000000000000000100111;
-
-
+//            long l1Px= 0b10000000000000000000001001;
+//            long l2Px= 0b100000000000000000001000111;
+//            long l3Px= 0b1000000000000000000000100111;
 
 
-//        CryptoAnalysis.breakGeffe(l1Px,l2Px,l3Px,CryptoAnalysis.zi);
+
+
+        CryptoAnalysis.breakGeffe(l1Px,l2Px,l3Px,CryptoAnalysis.zi);
 
         LinearFeedbackShiftRegister p1 = new LinearFeedbackShiftRegister(l1Px,17451593);
         LinearFeedbackShiftRegister p2 = new LinearFeedbackShiftRegister(l2Px,22162887);
